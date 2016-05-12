d33f25dff7baf6f8522bc3c25fec68c69e968f12
Marco L <mloiodice13@gmail.com>
2016-05-12 09:45:08 +0200

    complete Scorer class

--- src/fr/univ/lyon1/mastermind/Scorer.java
+++ src/fr/univ/lyon1/mastermind/Scorer.java
@@ -1,39 +1,54 @@
 package fr.univ.lyon1.mastermind;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scorer {
 	private final Code secret;
-	
+
 	public Scorer(Code secret) {
 		super();
 		this.secret = secret;
 	}
-	
+
 	public Score score(Code guess) {
-		return score(guess,secret);
+		return score(guess, secret);
 	}
 
 	public static Score score(Code guess, Code secret) {
-			int blackCount = exactMatches(guess,secret);
-			int whiteCount = matches(guess,secret) - blackCount;
-			return Score.valueOf(blackCount, whiteCount);
+		int blackCount = exactMatches(guess, secret);
+		int whiteCount = matches(guess, secret) - blackCount;
+		return Score.valueOf(blackCount, whiteCount);
 	}
 
 	public int getCodeLength() {
 		return secret.length();
 	}
-	
+
 	private static int exactMatches(Code guess, Code secret) {
-		//TODO: à compléter
-		
-		return whiteHits;
+		// TODO: à compléter
+		int blackHits = 0;
+
+		for (int i = 0; i < secret.asArray().length; i++) {
+			if (secret.asArray()[i].compareTo(guess.asArray()[i]) == 0)
+				blackHits++;
+		}
+
+		return blackHits;
 	}
 
 	private static int matches(Code guess, Code secret) {
-		//TODO: à compléter
-		
+		int matchesCount = 0;
+
+		List<Integer> removedIndex = new ArrayList<Integer>();
+		for (int i = 0; i < secret.length(); i++) {
+			for (int j = 0; j < guess.length(); j++) {
+				if (guess.asArray()[j] == secret.asArray()[i] && !(removedIndex.contains(i))) {
+					matchesCount++;
+					removedIndex.add(i);
+				}
+			}
+		}
 		return matchesCount;
 	}
 
